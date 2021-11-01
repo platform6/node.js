e5f6888f8127e83df594c92261894c49d969a342
garrett.peter.conn@gmail.com <36059331+platform6@users.noreply.github.com>
2021-11-01 11:32:37 -0400

    adding non mutating stort

---
+++ return-a-sorted-array-without-changing-the-original-array.js
@@ -0,0 +1,13 @@
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+let newArr = arr.concat()
+
+return newArr.sort(function(a, b) {
+  return a - b;
+});
+  // Only change code above this line
+}
+
+console.log(nonMutatingSort(globalArray));
